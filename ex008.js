15bb65b8b04309aae8a58fde08a80ac8abbe2d84
othonss <othon.santos.silva@gmail.com>
2022-08-14 20:37:20 -0300

    Módulo D Aula 11 Condições em Javascript Nesta aula foi utilizado estruturas condicionais simples e compostas

---
+++ ex008.js
@@ -0,0 +1,6 @@
+console.log('** EXERCÍCIO EXEMPLO DE ESTRUTURA CONCIDIONAL SIMPLES **')
+var velocidade = 65
+if(velocidade > 60){
+    console.log('Velocidade acima da permitida. Levou multa!')
+}
+console.log('** FIM DO EXERCÍCIO **')
---
+++ ex009.js
@@ -0,0 +1,8 @@
+console.log('** EXERCÍCIO EXEMPLO DE ESTRUTURA CONCIDIONAl COMPOSTA**')
+var pais = 'Japão'
+console.log(`Vivendo no(a) ${pais}`)
+if(pais != 'Brasil'){
+    console.log('Você é estrangeiro.')
+}else{
+    console.log('Você é brasileiro.')
+}
